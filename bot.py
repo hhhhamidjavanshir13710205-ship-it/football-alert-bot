767126abe66db54d322538a659c55a404249eece
hhhhamidjavanshir13710205-ship-it <hhhhamidjavanshir13710205@gmail.com>
2026-09-16 08:53:49 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,16 @@
+import os
+import requests
 
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
+
+def send_message(text):
+    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+    response = requests.post(
+        url,
+        data={"chat_id": CHAT_ID, "text": text}
+    )
+    print(response.text)
+
+if __name__ == "__main__":
+    send_message("⚽ ربات فوتبال با موفقیت فعال شد!")
